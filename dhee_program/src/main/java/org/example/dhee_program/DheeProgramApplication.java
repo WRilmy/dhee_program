2582461c0e66eaf7821eb6d1a13271c6578a4b1c
Map1e <120364274+WRilmy@users.noreply.github.com>
2024-07-22 18:40:46 +0800

    Add files via upload

---
+++ dhee_program/src/main/java/org/example/dhee_program/DheeProgramApplication.java
@@ -0,0 +1,13 @@
+package org.example.dhee_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DheeProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DheeProgramApplication.class, args);
+    }
+
+}
---
+++ dhee_program/src/test/java/org/example/dhee_program/DheeProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dhee_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DheeProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
