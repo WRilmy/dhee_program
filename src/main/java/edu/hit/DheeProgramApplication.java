d70cd06d36cca3d664a420a0223ba003487dc178
Map1e <3412883097@qq.com>
2024-07-22 20:54:45 +0800

    Test2

--- src/main/java/edu/hit/DheeProgramApplication.java
+++ src/main/java/edu/hit/DheeProgramApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DheeProgramApplication {
 
     public static void main(String[] args) {
-        wr是我爹
+
         SpringApplication.run(DheeProgramApplication.class, args);
     }
 
