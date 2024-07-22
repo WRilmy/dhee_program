c2ab00e64431a35e01935fd6e64777198c562db3
Map1e <3412883097@qq.com>
2024-07-22 20:17:04 +0800

    Test1

---
+++ src/test/java/edu/hit/DheeProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DheeProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
